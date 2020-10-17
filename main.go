294e920a64d71d2611f7d8249a610f49daee3d37
Myrat92 <zjsegzhf@gmail.com>
2020-10-17 22:12:25 +0800

    配置log模块

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
 	_ "ExpiredReminder/routers"
 	"ExpiredReminder/src/task"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 func init() {
 	models.Init()
 	beego.BConfig.WebConfig.Session.SessionOn = true
+	logs.SetLogger("file", `{"filename":"logs/ExpiredReminder.log","maxdays":30,"color":true}`)
+	logs.EnableFuncCallDepth(true)
+	go task.Run()
 }
 
 func main() {
-	go task.Run()
 	beego.Run()
 }
 
